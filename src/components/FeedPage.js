ed51ba1bcbb3a0fd701f65b7d026ac7be8f0a336
mwood <matt@cratebind.com>
2018-05-16 18:23:21 -0500

    started adding animations

--- src/components/FeedPage.js
+++ src/components/FeedPage.js
@@ -5,19 +5,41 @@ import { gql } from 'apollo-boost';
 import CreatePage from './CreatePage';
 import ListPreview from './ListPreview';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import posed from 'react-pose';
+
+const sidebarProps = {
+  open: {
+    // x: '0%',
+    delayChildren: 300,
+    staggerChildren: 50,
+  },
+  closed: {
+    delay: 500,
+    staggerChildren: 20,
+    // x: '-100%',
+  },
+};
+
+const itemProps = {
+  open: { opacity: 1, y: 0 },
+  closed: { opacity: 0, y: 20 },
+};
+
+const ListWrapper = posed.div(sidebarProps);
+const PosedListPreview = posed(ListPreview)(itemProps);
 
 export default class FeedPage extends Component {
   render() {
     return (
       <Query query={FEED_QUERY}>
         {({ data, loading, error, refetch }) => {
-          if (loading) {
-            return (
-              <div className="flex w-100 h-100 items-center justify-center pt7">
-                <div>Loading ...</div>
-              </div>
-            );
-          }
+          // if (loading) {
+          //   return (
+          //     <div className="flex w-100 h-100 items-center justify-center pt7">
+          //       <div>Loading ...</div>
+          //     </div>
+          //   );
+          // }
 
           if (error) {
             return (
@@ -27,8 +49,6 @@ export default class FeedPage extends Component {
             );
           }
 
-          console.log(data);
-
           return (
             <Grid>
               <Row>
@@ -36,13 +56,12 @@ export default class FeedPage extends Component {
                   {/* <h1>Lists</h1> */}
                   <CreatePage />
                   <Row>
-                    {data.listFeed &&
-                      data.listFeed.map((list, index) => (
-                        <Col className="box" lg={4} md={6} xs={12}>
-                          <ListPreview list={list} index={index} />
-                        </Col>
-                      ))}
-                    {this.props.children}
+                    <ListWrapper pose={loading ? 'closed' : 'open'}>
+                      {data.listFeed &&
+                        data.listFeed.map((list, index) => (
+                          <PosedListPreview list={list} index={index} />
+                        ))}
+                    </ListWrapper>
                   </Row>
                 </Col>
               </Row>
--- src/components/ListPreview.js
+++ src/components/ListPreview.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Col } from 'react-flexbox-grid';
 
-const ListPreview = ({ list, index }) => {
+const ListPreview = ({ list, index, hostRef }) => {
   const colors = [
     'rgba(97, 189, 79, 0.5)',
     'rgba(242,214,0, 0.5)',
     'rgba(255,171,74,0.5)',
     'rgba(235,90,70,0.5)',
     'rgba(195,119,224,0.5)',
-    'rgba(0,121,191,0.5)'
+    'rgba(0,121,191,0.5)',
   ];
 
   let colorIndex = index;
@@ -21,16 +22,20 @@ const ListPreview = ({ list, index }) => {
   const cardColor = [colors[colorIndex]];
 
   return (
-    <Link to={`/list/${list.id}`}>
-      <div className="ListPreview" style={{ backgroundColor: cardColor }}>
-        <div className="ListPreview__title">{list.title}</div>
-        <div className="ListPreview__items">
-          {list.items.map(item => (
-            <div className="ListPreview__item">{item.text}</div>
-          ))}
-        </div>
-      </div>
-    </Link>
+    <div ref={hostRef}>
+      <Col lg={4} md={6} xs={12}>
+        <Link to={`/list/${list.id}`}>
+          <div className="ListPreview" style={{ backgroundColor: cardColor }}>
+            <div className="ListPreview__title">{list.title}</div>
+            <div className="ListPreview__items">
+              {list.items.map(item => (
+                <div className="ListPreview__item">{item.text}</div>
+              ))}
+            </div>
+          </div>
+        </Link>
+      </Col>
+    </div>
   );
 };
 
